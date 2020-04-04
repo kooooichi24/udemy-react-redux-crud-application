b35f6165636e9409ffd55de074b0e477aa4cd319
古川広一 <ex.kouichi1224@gmail.com>
2020-04-04 22:42:38 +0900

    3-15/learn props of component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
 
 function App() {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 13 },
+    { name: "Koichi" },
+    { }
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   )
 }
 
-function Cat() {
-  return <div>Meow!</div>
+function User(props) {
+  return <div>Hi! I am {props.name}, and {props.age} years old.</div>
+}
+
+User.defaultProps = {
+  name: "Noname",
+  age: 1
 }
 
 export default App;
