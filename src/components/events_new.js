efed83c0173348e0a7e96237964b08f9cf5b9726
古川広一 <ex.kouichi1224@gmail.com>
2020-04-08 15:56:06 +0900

    small change

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
-
 import { postEvent } from '../actions';
 
 class EventsNew extends Component {
